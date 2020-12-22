b47ddabb3b914efc5839ee8a2bcb2733539e08f9
chenwujie <chenwujie@pwrd.com>
2020-12-22 22:01:48 +0800

    delete print

--- routers/api/user.go
+++ routers/api/user.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"github.com/unknwon/com"
 
@@ -12,7 +10,6 @@ import (
 // @Router /api/v1/user [get]
 func GetUser(c *gin.Context) {
 	id := com.StrTo(c.Param("id")).MustInt()
-	fmt.Println(id)
 	var user = models.User{}
 	result := user.GetUser(id)
 	c.JSON(200, result)
